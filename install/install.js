fa48430da09a6a37dfdbef6e154e0599bd9c1345
Sam Auciello <olleicua@gmail.com>
2015-07-11 18:23:28 -0400

    documentation

--- install/install.js
+++ install/install.js
@@ -20,21 +20,30 @@
   }
 
   window.INSTALL_RRSSB = {
+
+    // an array of registered button generating templates
     buttons: [],
+
+    // build up a string with an li in it for each registered button
     buildButtons: function() {
       var options = this.options
       return map(this.buttons, function(buttonTemplate) {
         return buttonTemplate(options);
       }).join('');
     },
+
+    // generate the uri that will be shared
     shareUri: function() {
       if (this.options.advancedOptions &&
           this.options.advancedOptions.keepUrlParameters) {
+        // include url parameters (e.g. 'example.com?parameter=value'
         return window.location.href;
       } else {
         return window.location.protocol + '//' + window.location.hostname + window.location.pathname;
       }
     },
+
+    // add the share uri and a url encoded version of it to the options
     processOptions: function() {
       var shareUri = this.shareUri();
       return extend({}, this.options, {
@@ -42,9 +51,13 @@
         uriEncodedShareUri: encodeURIComponent(shareUri)
       });
     },
+
+    // add a button template function to the array of registered buttons
     register: function(name, template) {
       this.buttons.push(template);
     },
+
+    // add the buttons to the DOM
     init: function(element, options) {
       this.options = options;
       if (!element) {
